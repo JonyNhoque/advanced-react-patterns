3ed7c33cf00453aff74d5554719924ab20351b24
JonyNhoque <jgabriel.nhoque@gmail.com>
2022-05-15 18:43:41 -0300

    feat: React Router added and setup. Created ContextModule component.

---
+++ advanced-react-patterns/src/components/context-module-functions/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './index.css'
+
+export const ContextModule = () => {
+    
+    return (
+        <div id='contextModule__container'>
+            
+        </div>
+    )
+}
--- advanced-react-patterns/src/index.tsx
+++ advanced-react-patterns/src/index.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import { ContextModule } from './components/context-module-functions';
 
 
 
@@ -9,7 +15,10 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App/>} />
+      <Route path='/context-module' element={<ContextModule/>} />
+    </Routes>
+  </BrowserRouter>
 );
