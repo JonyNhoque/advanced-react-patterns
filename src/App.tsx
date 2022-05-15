d6dd7cd10ea9b7cc95fe6d022b4ebbac4b8f1ab5
JonyNhoque <jgabriel.nhoque@gmail.com>
2022-05-15 20:01:21 -0300

    feat: created route and app link to challenge #1.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { Button } from './components/button';
+
+
+function App() {
+  return (
+    <div className="App__container">
+      <Button route='/context-module'></Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/button/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './style.css'
+
+
+export const Button = (props: any) => {
+    return (
+        <div className='button__container'>
+            <a href={props.route} className='button__body'>Cliquei aqui!</a>
+        </div>
+    )
+}
