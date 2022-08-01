64013ea6f9c9abe2be4a83fe60e2760d15731072
JonyNhoque <jgabriel.nhoque@gmail.com>
2022-08-01 12:16:57 -0300

    feat(basics): Created card component to contain future components and functionalities

--- src/pages/basics/Card/Card.tsx
+++ src/pages/basics/Card/Card.tsx
@@ -1,14 +1,15 @@
 import React from "react";
+import './card.css'
 
-export const Card = () => {
+export const Card = (props: any) => {
     return (
         <div className='card__container'>
             <div className='card__body'>
                 <div className='card__colorTag'>
-                    
+                    {props.cardName}
                 </div>
                 <div className='card__content'>
-                    Conteúdo
+                    <strong>{props.cardContent}</strong>
                 </div>
             </div>
         </div>
--- src/pages/basics/index.tsx
+++ src/pages/basics/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import './index.css'
+import { Card } from "./Card/Card";
 
 export const Basics = () => {
     return (
         <div className='basics__container'>
             <div className='basics__body'>
-                
+                <Card cardName='# 01 - Componente com filhos' />
             </div>
         </div>
     )
